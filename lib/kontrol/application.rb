380a7146e15256aee176b0ea1ee3cc3aba6e408c
Matthias Georgi <matti.georgi@gmail.com>
2009-01-07 02:47:15 +0100

    fixed content-length error

--- lib/kontrol/application.rb
+++ lib/kontrol/application.rb
@@ -121,7 +121,7 @@ module Kontrol
       response.status = status if response.status.nil?
       response.header.merge!(header)
       response.body = body if response.body.empty?
-      response['Content-Length'] = response.body.size.to_s
+      response['Content-Length'] = response.body.size.to_s if response.status == 200
       response['Content-Type'] = guess_content_type if response['Content-Type'].empty?
 
       response.finish
